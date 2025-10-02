6773931992b6a6b2dc40c6eca78d7f658997e511
Andrew Thomas <aliveandcare6@gmail.com>
2025-10-02 13:52:25 -0500

    corrected ticketmaster api code

--- scraper/nashville/spiders/ticketmaster_spider.py
+++ scraper/nashville/spiders/ticketmaster_spider.py
@@ -1,19 +1,15 @@
 import scrapy
 import os
 from urllib.parse import urlencode
-from nashville.items import BusinessItem # Corrected Item
-
+from nashville.items import BusinessItem
 class TicketmasterSpider(scrapy.Spider):
     name = 'ticketmaster'
     base_url = 'https://app.ticketmaster.com/discovery/v2/events.json'
-
     def start_requests(self):
         api_key = os.getenv('TICKETMASTER_API_KEY')
         if not api_key:
-            # We don't raise an error anymore, just log it so it doesn't crash everything
             self.logger.error("TICKETMASTER_API_KEY not found in environment variables.")
             return
-
         params = {
             'apikey': api_key,
             'dmaId': '343',
@@ -22,36 +18,28 @@ class TicketmasterSpider(scrapy.Spider):
         }
         url = f"{self.base_url}?{urlencode(params)}"
         yield scrapy.Request(url=url, callback=self.parse)
-
     def parse(self, response):
-        # We add a check for the 401 Unauthorized error
         if response.status == 401:
             self.logger.error("Ticketmaster API request failed (401 Unauthorized). Check your API key.")
             return
-
         data = response.json()
-
         if '_embedded' not in data or 'events' not in data['_embedded']:
             self.logger.warning("No events found in Ticketmaster API response for Nashville.")
             return
-
         events = data['_embedded']['events']
         for event in events:
-            item = BusinessItem() # Corrected Item
-
+            item = BusinessItem()
             item['source'] = 'ticketmaster'
             item['name'] = event.get('name')
             item['url'] = event.get('url')
             item['event_id'] = event.get('id')
-            item['description'] = event.get('info') or event.get('pleaseNote')
-            
+            item['description'] = event.get('info') or event.get('pleaseNote')            
             try:
                 event_date = event['dates']['start'].get('localDate')
                 event_time = event['dates']['start'].get('localTime', '')
                 item['event_date'] = f"{event_date} {event_time}".strip()
             except KeyError:
                 item['event_date'] = None
-
             try:
                 venue = event['_embedded']['venues'][0]
                 item['venue_name'] = venue.get('name')
@@ -60,7 +48,6 @@ class TicketmasterSpider(scrapy.Spider):
             except (KeyError, IndexError):
                 item['venue_name'] = None
                 item['venue_city'] = None
-                item['venue_address'] = None
-            
+                item['venue_address'] = None            
             item['neighborhood'] = None
             yield item
