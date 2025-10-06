4ccade9cbc9a89d52322ff4c89ab81488069964b
Andrew Thomas <aliveandcare6@gmail.com>
2025-10-06 12:52:12 -0500

    removed duplicate folders/files

--- scraper/scraper/nashville/settings.py
+++
@@ -1,7 +0,0 @@
-SPIDER_MODULES = ['nashville.spiders']
-DOWNLOAD_HANDLERS = {
-    "http": "scrapy_playwright_stealth.handler.ScrapyPlaywrightStealthDownloadHandler",
-    "https": "scrapy_playwright_stealth.handler.ScrapyPlaywrightStealthDownloadHandler",
-}
-
-TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
